23297d3b3b9c92e13fd2d5ff51f8b90f4bb25f09
bihd00 <bihany.daniel@gmail.com>
2022-12-28 17:40:13 +0100

    generator - update UI, add file upload

--- frontend/src/components/generator/generator.svelte
+++ frontend/src/components/generator/generator.svelte
@@ -2,9 +2,12 @@
   import IfUser from "../users/if-user.svelte";
   import { generator, type Generator } from "../../stores/generator";
   import { generateImages } from "../../services/firebase";
+  import FileUploadButton from "../ui/file-upload-button.svelte";
 
   let currentGenerator: Generator;
   let textEl: HTMLTextAreaElement;
+  let file: File;
+
   let confirmation: string;
   let loading = false;
   let error: string;
@@ -24,12 +27,20 @@
   async function handleSubmit() {
     loading = true;
     const text = textEl.value;
+    if (text == null || text.length < 10) return;
     const { serverError } = await generateImages(text);
     if (serverError) error = serverError;
     generator.subscribe((gen) => {
       currentGenerator = gen;
     });
   }
+
+  async function handleUpload() {
+    if (file == null || file.type !== "text/plain") return;
+    const text = await file.text();
+    textEl.value = text;
+  }
+
   $: images = currentGenerator?.images || [];
   $: $generator, reset();
   $: buttonText = confirmation
@@ -43,30 +54,55 @@
 
 <IfUser>
   <section
-    class="container mx-auto mt-8 flex flex-col items-center justify-center h-[76vh] rounded-lg bg-zinc-900"
+    class="container mx-auto mt-8 lg:h-[76vh] rounded-lg bg-zinc-900 p-4 flex flex-col items-stretch"
   >
-    <div class="h-full w-full sm:h-2/3 sm:w-2/3 sm:mb-24 xl:w-1/2">
-      <form
-        on:submit|preventDefault={handleSubmit}
-        class="flex flex-col gap-4 h-full"
+    <h1 class="mb-4">Dashboard</h1>
+    <div class="lg:grid lg:grid-cols-4 lg:gap-4 lg:grid-rows-1 lg:h-full">
+      <!--  -->
+      <div
+        class="lg:pr-4 lg:border-r-2 lg:border-solid lg:border-zinc-800 mb-8 lg:mb-0"
       >
-        <label for="text">Enter your cover text</label>
-        <textarea bind:this={textEl} name="text" id="text" class="h-full" />
-        <div class="">
-          <button
-            class="border-solid border-2 border-white px-8 py-2"
-            type="submit"
-            >{buttonText}
-          </button>
+        <h2 class="text-2xl mb-2">Create</h2>
+        <div class="mb-4">
+          <FileUploadButton
+            label="Upload a Text File"
+            bind:file
+            on:upload={handleUpload}
+          />
         </div>
-      </form>
+        <form on:submit|preventDefault={handleSubmit} class="flex flex-col">
+          <label for="text" class="mb-2">Enter text manually</label>
+          <textarea
+            bind:this={textEl}
+            name="text"
+            id="text"
+            rows="10"
+            class="mb-3 bg-zinc-900 border-2 border-solid border-zinc-700 p-2"
+          />
+          <div class="">
+            <button class="bg-sky-400 px-8 py-2 text-black font-bold"
+              >{buttonText}
+            </button>
+          </div>
+        </form>
+      </div>
+      <!--  -->
+      <div class="lg:col-span-3">
+        <h2 class="text-2xl mb-2">Results</h2>
+        <div class="flex columns-auto gap-8 flex-wrap">
+          {#if images.length}
+            {#each images as img, i}
+              <img
+                id={`${img.refId}-img-${i}`}
+                src={img.url}
+                alt="Generated..."
+              />
+            {/each}
+          {:else}
+            <p class="text-gray-500">...no results yet 😐</p>
+          {/if}
+        </div>
+      </div>
     </div>
   </section>
-  {#if images}
-    <section class="container mx-auto mt-8 flex">
-      {#each images as img, i}
-        <img id={`${img.refId}-img-${i}`} src={img.url} alt="Generated..." />
-      {/each}
-    </section>
-  {/if}
 </IfUser>
---
+++ frontend/src/components/ui/file-upload-button.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  import FileUpload from "../../assets/file-upload.svg";
+
+  export let label = "Upload a File";
+  export let file: File;
+  let fileInput: HTMLInputElement;
+  let filename: string;
+  let isDragging = false;
+
+  const dispatch = createEventDispatcher();
+  function onDragenter() {
+    isDragging = true;
+  }
+  function onDragLeave() {
+    isDragging = false;
+  }
+  function setFile() {
+    if (fileInput.files.length < 1) return;
+    file = fileInput.files[0];
+    if (file.name) {
+      filename = file.name;
+    }
+    dispatch("upload");
+  }
+  $: isSet = file != null;
+</script>
+
+<label
+  for="text-file"
+  class="relative flex min-w-full justify-center gap-1 
+         self-center items-center p-3 py-4 
+         bg-zinc-800 mb-2 hover:bg-neutral-700
+         {isDragging && !isSet ? 'border-2 border-dotted border-white' : ''}"
+>
+  <img src={FileUpload} alt="Upload a file..." />
+  <span>{filename ? filename : label}</span>
+  <input
+    bind:this={fileInput}
+    on:change={setFile}
+    on:dragenter={onDragenter}
+    on:dragleave={onDragLeave}
+    class="cursor-pointer absolute top-0 
+            left-0 w-full h-full opacity-0 d-block"
+    id="text-file"
+    type="file"
+  />
+</label>
