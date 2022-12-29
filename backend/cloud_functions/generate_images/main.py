c25e08af300f3fa8c1cda67314d7ea6669ab5ac7
bihd00 <bihany.daniel@gmail.com>
2022-12-29 12:25:42 +0100

    feat(images): persistent storage with GCS

--- backend/cloud_functions/generate_images/main.py
+++ backend/cloud_functions/generate_images/main.py
@@ -1,8 +1,11 @@
-from google.cloud import firestore
+from google.cloud import firestore, storage
 from dataclasses import dataclass
-from typing import Union, List
+from typing import Optional, List, Any
+from base64 import b64decode
+from enum import Enum
 import datetime
 import openai
+import uuid
 import os
 
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -12,24 +15,34 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 # Creates a new record in /images/{imageId}
 def generate_images(data, ctx) -> None:
     db = firestore.Client()
+    
     # get fields from new record
     fields = data['value']['fields']
     # fields must include refId & content
     if 'refId' not in fields or 'content' not in fields:
         return
+
     # check whether refId could be obtained as a string
     content = fields['refId']
     refId = content['stringValue']
     # if not, return
     if not refId:
         return
+
     # same goes for content
     content = fields['content']
     value = content['stringValue']
     if not value:
         return
+    
     # get generated images
-    images = _generate_images_with_open_ai(prompt=value)
+    images = _generate_images_with_open_ai(prompt=value, format=ImageFormat.B64)
+    
+    # if using openai b64_json output, update image URLs via upload to GCS
+    gcs_client = storage.Client()
+    for image in images:
+        _add_img_url_via_to_gcs(image, client=gcs_client)
+
     # create records in images collection
     batch = db.batch()
     for image in images:
@@ -43,21 +56,65 @@ def generate_images(data, ctx) -> None:
 
 @dataclass
 class Image:
-    url: Union[str, None]
-    refId: Union[str, None] = None
-    createdAt: Union[str, None] = None
+    url: Optional[str] = None
+    b64_json: Optional[Any] = None
+    refId: Optional[str] = None
+    createdAt: Optional[str] = None
 
     @property
     def data(self):
-        return self.__dict__
+        return { k:v for k,v in self.__dict__.items() if k not in ['b64_json']}
 
 
-def _generate_images_with_open_ai(prompt: str) -> List[Image]:
+class ImageFormat(Enum):
+    URL = 'url'
+    B64 = 'b64_json'
+
+
+def _generate_images_with_open_ai(
+    prompt: str, 
+    format: ImageFormat = ImageFormat.URL 
+) -> List[Image]:
     size='256'
     response = openai.Image.create(
         prompt=prompt,
+        size=f"{size}x{size}",
         n=2,
-        size=f"{size}x{size}"
+        response_format=format.value
     )
     return list(map(lambda x: Image(**x), response['data']))
+
+
+def _add_img_url_via_to_gcs(image: Image, client: storage.Client = None) -> None:
+    """
+    - Uploads a b64 encoded PNG image to google cloud storage.
+    - Sets image.url to its uploaded location on GCS.
+        
+        returns:
+            public url (str)
+    """
+    if image.url:
+        print('Image already contains url. Skipping')
+        return
+
+    if not image.b64_json:
+        print('Image does not contain b64_json. Skipping')
+        return
+
+    storage_client = client if client else \
+        storage.Client()
+    image_name_for_upload = 'images/' + str(uuid.uuid4()) + '.png'
+    image_base64 = b64decode(image.b64_json)
+    
+    bucket = storage_client.bucket(bucket_name=os.environ['GCS_BUCKET'])
+    blob = bucket.blob(blob_name=image_name_for_upload)
+    blob.upload_from_string(image_base64, content_type='image/png')
+
+    try:
+        blob.make_public()
+    except Exception as e:
+        print(e)
+    
+    image.url = blob.public_url
+    return
     
--- backend/tests/cloud_functions.py
+++ backend/tests/cloud_functions.py
@@ -1,12 +1,15 @@
 from dotenv import dotenv_values
-from google.cloud import firestore
+from google.cloud import firestore, storage
 from dataclasses import dataclass
-from typing import Union, List
+from typing import Optional, List, Any
+from base64 import b64decode
 from pathlib import Path
+from enum import Enum
 import numpy as np
 import datetime
 import openai
 import json
+import uuid
 import os
 
 ROOT = Path(__file__).resolve().parent.parent
@@ -84,24 +87,33 @@ def test_summarize_text():
 # Creates a new record in /images/{imageId}
 def generate_images(data, ctx) -> None:
     db = firestore.Client.from_service_account_json(os.path.join(ROOT, 'service-account-key.json'))
+    
     # get fields from new record
     fields = data['value']['fields']
     # fields must include refId & content
     if 'refId' not in fields or 'content' not in fields:
         return
+
     # check whether refId could be obtained as a string
     content = fields['refId']
     refId = content['stringValue']
     # if not, return
     if not refId:
         return
+
     # same goes for content
     content = fields['content']
     value = content['stringValue']
     if not value:
         return
+    
     # get generated images
-    images = _generate_images_with_open_ai(prompt=value)
+    images = _generate_images_with_open_ai(prompt=value, format=ImageFormat.B64)
+    
+    # if using openai b64_json output, update image URLs via upload to GCS
+    for image in images:
+        _add_img_url_via_to_gcs(image)
+
     # create records in images collection
     batch = db.batch()
     for image in images:
@@ -115,23 +127,67 @@ def generate_images(data, ctx) -> None:
 
 @dataclass
 class Image:
-    url: Union[str, None]
-    refId: Union[str, None] = None
-    createdAt: Union[str, None] = None
+    url: Optional[str] = None
+    b64_json: Optional[Any] = None
+    refId: Optional[str] = None
+    createdAt: Optional[str] = None
 
     @property
     def data(self):
-        return self.__dict__
+        return { k:v for k,v in self.__dict__.items() if k not in ['b64_json']}
 
 
-def _generate_images_with_open_ai(prompt: str) -> List[Image]:
+class ImageFormat(Enum):
+    URL = 'url'
+    B64 = 'b64_json'
+
+
+def _generate_images_with_open_ai(
+    prompt: str, 
+    format: ImageFormat = ImageFormat.URL 
+) -> List[Image]:
     size='256'
     response = openai.Image.create(
         prompt=prompt,
+        size=f"{size}x{size}",
         n=2,
-        size=f"{size}x{size}"
+        response_format=format.value
     )
     return list(map(lambda x: Image(**x), response['data']))
+
+
+def _add_img_url_via_to_gcs(image: Image, client: storage.Client = None) -> None:
+    """
+    - Uploads a b64 encoded PNG image to google cloud storage.
+    - Sets image.url to its uploaded location on GCS.
+        
+        returns:
+            public url (str)
+    """
+    if image.url:
+        print('Image already contains url. Skipping')
+        return
+
+    if not image.b64_json:
+        print('Image does not contain b64_json. Skipping')
+        return
+
+    storage_client = client if client else \
+        storage.Client.from_service_account_json(os.path.join(ROOT, 'service-account-key.json'))
+    image_name_for_upload = 'images/' + str(uuid.uuid4()) + '.png'
+    image_base64 = b64decode(image.b64_json)
+    
+    bucket = storage_client.bucket(bucket_name=ENV['GCS_BUCKET'])
+    blob = bucket.blob(blob_name=image_name_for_upload)
+    blob.upload_from_string(image_base64, content_type='image/png')
+
+    try:
+        blob.make_public()
+    except Exception as e:
+        print(e)
+    
+    image.url = blob.public_url
+    return
     
 
 def test_generate_images():
@@ -139,6 +195,18 @@ def test_generate_images():
     data['value']['fields']['content']['stringValue'] = 'a dog riding a bycicle, with a sombrero on its head'
     context = json.load(open(os.path.join(ROOT, 'tests/data/summarize_text_context.json'), 'r'))
     assert generate_images(data, context) is None
+
+
+def test_generate_images_with_open_ai_url():
+    prompt = '4 helicopters having a picnic, playing cards. Friendly'
+    images = _generate_images_with_open_ai(prompt)
+    assert images[0].url is not None
+
+
+def test_generate_images_with_open_ai_b64_json():
+    prompt = 'telescope image of a planetary system, where ich planet is a fruit. Hundreds of planets, colorful, high-res'
+    images = _generate_images_with_open_ai(prompt, ImageFormat.B64)
+    assert images[0].b64_json is not None
 
 
 if __name__ == "__main__":
